77ab82c61dff2aaac1d1430b20697e47358f24af
Bakual <werbemails@bakual.ch>
2013-10-04 16:16:35 +0200

    Adding module to request a user report

---
+++ mod_wissensmatrix_userreport/mod_wissensmatrix_userreport.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package     Wissensmatrix
+ * @subpackage  mod_wissensmatrix_userreport
+ *
+ * @copyright   Copyright (C) 2013 Thomas Hunziker
+ * @license     GNU General Public License version 2
+ */
+
+defined('_JEXEC') or die;
+
+require JModuleHelper::getLayoutPath('mod_wissensmatrix_userreport', $params->get('layout', 'default'));
---
+++ mod_wissensmatrix_userreport/tmpl/default.php
@@ -0,0 +1,33 @@
+<?php
+defined('_JEXEC') or die;
+
+JHtml::_('bootstrap.tooltip');
+?>
+<form action="<?php echo JRoute::_('index.php'); ?>" method="post" id="userreport-form" class="form-inline">
+	<div class="userdata">
+		<div id="form-userreport-uid" class="control-group">
+			<div class="controls">
+				<?php if (!$params->get('usetext')) : ?>
+					<div class="input-prepend">
+						<span class="add-on">
+							<span class="icon-user hasTooltip" title="<?php echo JText::_('MOD_WISSENSMATRIX_USERREPORT_USERID_DESC') ?>"></span>
+							<label for="moduserreport-uid" class="element-invisible"><?php echo JText::_('MOD_WISSENSMATRIX_USERREPORT_USERID_LABEL'); ?></label>
+						</span>
+						<input id="moduserreport-uid" type="text" name="uid" class="input-small" tabindex="0" size="18" placeholder="<?php echo JText::_('MOD_WISSENSMATRIX_USERREPORT_PLACEHOLDER_USERNAME') ?>" />
+					</div>
+				<?php else: ?>
+					<label for="moduserreport-uid" class="hasTooltip" title="<?php echo JText::_('MOD_WISSENSMATRIX_USERREPORT_USERID_DESC') ?>"><?php echo JText::_('MOD_WISSENSMATRIX_USERREPORT_USERID_LABEL') ?></label>
+					<input id="moduserreport-uid" type="text" name="uid" class="input-small" tabindex="0" size="18" placeholder="<?php echo JText::_('MOD_WISSENSMATRIX_USERREPORT_PLACEHOLDER_USERNAME') ?>" />
+				<?php endif; ?>
+			</div>
+		</div>
+		<div id="form-login-submit" class="control-group">
+			<div class="controls">
+				<button type="submit" tabindex="0" name="Submit" class="btn btn-primary"><?php echo JText::_('MOD_WISSENSMATRIX_USERREPORT_REQUEST') ?></button>
+			</div>
+		</div>
+		<input type="hidden" name="option" value="com_wissensmatrix" />
+		<input type="hidden" name="task" value="userreport.request" />
+		<?php echo JHtml::_('form.token'); ?>
+	</div>
+</form>
