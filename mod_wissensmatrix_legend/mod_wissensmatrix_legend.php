618186159db9605087d837f759206c9f53499e60
Bakual <werbemails@bakual.ch>
2013-10-29 16:14:31 +0100

    Adding new legend module. Shows a legend for some reports.

---
+++ mod_wissensmatrix_legend/mod_wissensmatrix_legend.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @package     Wissensmatrix
+ * @subpackage  mod_wissensmatrix_userreport
+ *
+ * @copyright   Copyright (C) 2013 Thomas Hunziker
+ * @license     GNU General Public License version 2
+ */
+
+defined('_JEXEC') or die;
+
+$jinput	= JFactory::getApplication()->input;
+
+if ($jinput->get('option') != 'com_wissensmatrix')
+{
+	return;
+}
+
+$view	= $jinput->get('view');
+switch ($view)
+{ 
+	case 'reportfwiglevels':
+	case 'reportfwigteam':
+	case 'reportfwiteam':
+		$layout	= 'default';
+		break;
+	case 'reportfwiglevelssummary':
+		$layout	= 'summary';
+		break;
+	default:
+		return;
+}
+$moduleclass_sfx	= htmlspecialchars($params->get('moduleclass_sfx'));
+
+JHTML::addIncludePath(JPATH_SITE.'components/com_wissensmatrix/helpers');
+
+require JModuleHelper::getLayoutPath('mod_wissensmatrix_legend', $layout);
