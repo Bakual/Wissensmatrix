cdf07ac82b2f85aee79c98a4f5bfd9267c9250af
Thomas Hunziker <werbemails@bakual.ch>
2016-09-02 12:51:05 +0200

    Adding age to date of birth

--- mod_wissensmatrix_birthday/tmpl/default.php
+++ mod_wissensmatrix_birthday/tmpl/default.php
@@ -9,12 +9,13 @@
 
 defined('_JEXEC') or die;
 
+$year = date('Y');
 ?>
 <div class="wm-birthday<?php echo $moduleclass_sfx; ?>">
 	<dl>
 	<?php foreach ($list as $item): ?>
 		<dt><?php echo $item->vorname . ' ' . $item->name; ?></dt>
-		<dd><?php echo JHtml::date($item->geb, JText::_('DATE_FORMAT_LC4')); ?></dd>
+		<dd><?php echo JHtml::date($item->geb, JText::_('DATE_FORMAT_LC4')); ?> (<?php echo $year - JHtml::date($item->geb, 'Y'); ?>)</dd>
 	<?php endforeach; ?>
 	</dl>
 </div>
